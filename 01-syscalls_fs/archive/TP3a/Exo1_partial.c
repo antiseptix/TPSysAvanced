08554c1cae47bf8b2dc2a815ce087d47db6eb27d
LePingWin <loic.bine@gmail.com>
2018-03-23 13:29:15 +0100

    add 3a

--- 01-syscalls_fs/archive/TP3a/Exo1_partial.c
+++
@@ -1,8 +0,0 @@
-  sigaction(SIGINT,&action,0);
-
-  sigaddset(&sig_proc,SIGINT);
-  sigprocmask(SIG_SETMASK,&sig_proc,NULL);
-
-  sigfillset(&sig_proc);
-  sigdelset(&sig_proc,SIGINT);
-  sigsuspend(&sig_proc);
